a60de5a818917545ac003714ce813b901c55de44
Thanarat Tonpradit <auauone@gmail.com>
2020-05-18 01:41:34 +0700

    refactor layout for reuseable

---
+++ src/components/header.php
@@ -0,0 +1,19 @@
+<div class="main-header">
+  <!-- Logo Header -->
+  <div class="logo-header" data-background-color="blue">
+
+    <a href="/" class="logo">
+      <img src="https://via.placeholder.com/100x35" alt="navbar brand" class="navbar-brand">
+    </a>
+    <button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse" data-target="collapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon">
+        <i class="icon-menu"></i>
+      </span>
+    </button>
+  </div>
+  <!-- End Logo Header -->
+  <!-- Navbar Header -->
+  <nav class="navbar navbar-header" data-background-color="blue">
+  </nav>
+  <!-- End Navbar -->
+</div>
---
+++ src/components/sidebar.php
@@ -0,0 +1,190 @@
+	<!-- Sidebar -->
+	<div class="sidebar sidebar-style-2">
+	  <div class="sidebar-wrapper scrollbar scrollbar-inner">
+	    <div class="sidebar-content">
+	      <div class="user">
+	        <div class="avatar-sm float-left mr-2">
+	          <img src="https://via.placeholder.com/50" alt="..." class="avatar-img rounded-circle">
+	        </div>
+	        <div class="info">
+	          <a data-toggle="collapse" href="#collapseExample" aria-expanded="true">
+	            <span>
+	              Hizrian
+	              <span class="user-level">Administrator</span>
+	              <span class="caret"></span>
+	            </span>
+	          </a>
+	          <div class="clearfix"></div>
+
+	          <div class="collapse in" id="collapseExample">
+	            <ul class="nav">
+	              <li>
+	                <a href="#logout">
+	                  <span class="link-collapse">Logout</span>
+	                </a>
+	              </li>
+	            </ul>
+	          </div>
+	        </div>
+	      </div>
+	      <ul class="nav nav-primary">
+	        <li class="nav-item">
+	          <a data-toggle="collapse" href="#setting">
+	            <i class="fas fa-layer-group"></i>
+	            <p>Menu1</p>
+	            <span class="caret"></span>
+	          </a>
+	          <div class="collapse" id="setting">
+	            <ul class="nav nav-collapse">
+	              <li>
+	                <a href="#">
+	                  <span class="sub-item">submenu1</span>
+	                </a>
+	              </li>
+	              <li>
+	                <a href="#">
+	                  <span class="sub-item">submenu2</span>
+	                </a>
+	              </li>
+	              <li>
+	                <a href="#">
+	                  <span class="sub-item">submenu3</span>
+	                </a>
+	              </li>
+	              <li>
+	                <a href="#">
+	                  <span class="sub-item">submenu4</span>
+	                </a>
+	              </li>
+	              <li>
+	                <a href="#">
+	                  <span class="sub-item">submenu5</span>
+	                </a>
+	              </li>
+	            </ul>
+	          </div>
+	        </li>
+	        <li class="nav-item">
+	          <a data-toggle="collapse" href="#sidebarLayouts">
+	            <i class="fas fa-th-list"></i>
+	            <p>Menu2</p>
+	            <span class="caret"></span>
+	          </a>
+	          <div class="collapse" id="sidebarLayouts">
+	            <ul class="nav nav-collapse">
+	              <li>
+	                <a href="#">
+	                  <span class="sub-item">submenu1</span>
+	                </a>
+	              </li>
+	              <li>
+	                <a href="#">
+	                  <span class="sub-item">submenu2</span>
+	                </a>
+	              </li>
+	              <li>
+	                <a href="#">
+	                  <span class="sub-item">submenu3</span>
+	                </a>
+	              </li>
+	            </ul>
+	          </div>
+	        </li>
+	        <li class="nav-item">
+	          <a data-toggle="collapse" href="#forms">
+	            <i class="fas fa-pen-square"></i>
+	            <p>Menu3</p>
+	            <span class="caret"></span>
+	          </a>
+	          <div class="collapse" id="forms">
+	            <ul class="nav nav-collapse">
+	              <li>
+	                <a href="#">
+	                  <span class="sub-item">submenu1</span>
+	                </a>
+	              </li>
+	              <li>
+	                <a href="#">
+	                  <span class="sub-item">submenu2</span>
+	                </a>
+	              </li>
+	              <li>
+	                <a href="#">
+	                  <span class="sub-item">submenu3</span>
+	                </a>
+	              </li>
+	            </ul>
+	          </div>
+	        </li>
+	        <li class="nav-item">
+	          <a data-toggle="collapse" href="#tables">
+	            <i class="fas fa-table"></i>
+	            <p>Menu4</p>
+	            <span class="caret"></span>
+	          </a>
+	          <div class="collapse" id="tables">
+	            <ul class="nav nav-collapse">
+	              <li>
+	                <a href="#">
+	                  <span class="sub-item">submenu1</span>
+	                </a>
+	              </li>
+	              <li>
+	                <a href="#l">
+	                  <span class="sub-item">submenu2</span>
+	                </a>
+	              </li>
+	            </ul>
+	          </div>
+	        </li>
+	        <li class="nav-item">
+	          <a data-toggle="collapse" href="#maps">
+	            <i class="fas fa-map-marker-alt"></i>
+	            <p>Menu5</p>
+	            <span class="caret"></span>
+	          </a>
+	          <div class="collapse" id="maps">
+	            <ul class="nav nav-collapse">
+	              <li>
+	                <a href="#">
+	                  <span class="sub-item">submenu1</span>
+	                </a>
+	              </li>
+	              <li>
+	                <a href="#">
+	                  <span class="sub-item">submenu2</span>
+	                </a>
+	              </li>
+	              <li>
+	                <a href="#">
+	                  <span class="sub-item">submenu3</span>
+	                </a>
+	              </li>
+	              <li>
+	                <a href="#">
+	                  <span class="sub-item">submenu4</span>
+	                </a>
+	              </li>
+	              <li>
+	                <a href="#">
+	                  <span class="sub-item">submenu5</span>
+	                </a>
+	              </li>
+	              <li>
+	                <a href="#">
+	                  <span class="sub-item">submenu6</span>
+	                </a>
+	              </li>
+	              <li>
+	                <a href="#">
+	                  <span class="sub-item">submenu7</span>
+	                </a>
+	              </li>
+	            </ul>
+	          </div>
+	        </li>
+	      </ul>
+	    </div>
+	  </div>
+	</div>
+	<!-- End Sidebar -->
