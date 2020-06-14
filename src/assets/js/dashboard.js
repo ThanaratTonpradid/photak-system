14728c36ff9f9a60d2e23af09c0a23cae9279e87
Thanarat Tonpradit <thanarat@dollarsign.co>
2020-06-15 02:07:14 +0700

    add sideber for setting menu system

--- src/assets/js/dashboard.js
+++ src/assets/js/dashboard.js
@@ -1,6 +1,29 @@
-/* globals Chart:false, feather:false */
+/* enable lib feather icon */
 
 (function () {
   'use strict'
   feather.replace()
 }())
+
+/* sidebar script */
+$(function() {
+  const pathname = window.location.pathname;
+  if (pathname === '/pages/position.php') {
+    $('#position-page').addClass('active')
+    $('#setting-submenu').collapse('show')
+  } else if (pathname === '/pages/department.php') {
+    $('#department-page').addClass('active')
+    $('#setting-submenu').collapse('show')
+  } else if (pathname === '/pages/employee.php') {
+    $('#employee-page').addClass('active')
+    $('#setting-submenu').collapse('show')
+  } else if (pathname === '/pages/building.php') {
+    $('#building-page').addClass('active')
+    $('#setting-submenu').collapse('show')
+  } else if (pathname === '/pages/room.php') {
+    $('#room-page').addClass('active')
+    $('#setting-submenu').collapse('show')
+  } else {
+    $('#home-menu').addClass('active')
+  }
+});
--- src/components/sidebar.php
+++ src/components/sidebar.php
@@ -2,37 +2,64 @@
   <div class="sidebar-sticky pt-3">
     <ul class="nav flex-column">
       <li class="nav-item">
-        <a class="nav-link active" href="#">
+        <a class="nav-link" id="home-menu" href="/index.php">
           <span data-feather="home"></span>
-          Dashboard <span class="sr-only">(current)</span>
+          Dashboard
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">
-          <span data-feather="file"></span>
+        <a class="nav-link" id="setting-menu" role="button" data-toggle="collapse" data-target="#setting-submenu" aria-expanded="true" aria-controls="setting-submenu">
+          <span data-feather="settings"></span>
           ตั้งค่าระบบ
         </a>
+        <ul class="nav flex-column ml-5 collapse" id="setting-submenu" aria-labelledby="setting-menu" data-parent="#sidebarMenu">
+          <li class="nav-item">
+            <a id="position-page" class="nav-link" href="/pages/position.php">
+              จัดการต่ำแหน่งผู้ใช้งาน
+            </a>
+          </li>
+          <li class="nav-item">
+            <a id="department-page" class="nav-link" href="/pages/department.php">
+              จัดการแผนก
+            </a>
+          </li>
+          <li class="nav-item">
+            <a id="employee-page" class="nav-link" href="/pages/employee.php">
+              จัดการผู้ใช้งาน
+            </a>
+          </li>
+          <li class="nav-item">
+            <a id="building-page" class="nav-link" href="/pages/building.php">
+              จัดการอาคาร
+            </a>
+          </li>
+          <li class="nav-item">
+            <a id="room-page" class="nav-link" href="/pages/room.php">
+              จัดการห้อง
+            </a>
+          </li>
+        </ul>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">
+        <a class="nav-link" id="product-menu" role="button" data-toggle="collapse" data-target="#product-submenu" aria-expanded="false" aria-controls="product-submenu">
           <span data-feather="shopping-cart"></span>
           ระบบจัดการวัสดุ-ครุภัณฑ์
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">
+        <a class="nav-link" id="repair-menu" role="button" data-toggle="collapse" data-target="#repair-submenu" aria-expanded="false" aria-controls="repair-submenu">
           <span data-feather="users"></span>
           ระบบแจ้งซ่อม
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">
+        <a class="nav-link" id="material-menu" role="button" data-toggle="collapse" data-target="#material-submenu" aria-expanded="false" aria-controls="material-submenu">
           <span data-feather="bar-chart-2"></span>
           ระบบเบิกวัสดุ
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">
+        <a class="nav-link" id="report-menu" role="button" data-toggle="collapse" data-target="#report-submenu" aria-expanded="false" aria-controls="report-submenu">
           <span data-feather="layers"></span>
           ระบบรายงาน
         </a>
