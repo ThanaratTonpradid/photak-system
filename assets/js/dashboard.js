240137876478628f302a555563560097dca5fe43
Thanarat Tonpradit <auauone@gmail.com>
2020-07-19 22:23:36 +0700

    fix adtive menu bug

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -8,70 +8,72 @@
 /* sidebar script */
 $(function() {
   const pathname = window.location.pathname;
-  if (/position/ig.test(pathname)) {
+  console.log(window.location);
+  console.log(pathname);
+  if (pathname === '/photak-system/pages/position.php') {
     $('#position-page').addClass('active')
     $('#setting-submenu').collapse('show')
-  } else if (/department/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/department.php') {
     $('#department-page').addClass('active')
     $('#setting-submenu').collapse('show')
-  } else if (/employee/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/employee.php') {
     $('#employee-page').addClass('active')
     $('#setting-submenu').collapse('show')
-  } else if (/building/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/building.php') {
     $('#building-page').addClass('active')
     $('#setting-submenu').collapse('show')
-  } else if (/room/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/room.php') {
     $('#room-page').addClass('active')
     $('#setting-submenu').collapse('show')
-  } else if (/material-type/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/material-type.php') {
     $('#material-type-page').addClass('active')
     $('#product-submenu').collapse('show')
-  } else if (/product-type/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/product-type.php') {
     $('#product-type-page').addClass('active')
     $('#product-submenu').collapse('show')
-  } else if (/material/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/material.php') {
     $('#material-page').addClass('active')
     $('#product-submenu').collapse('show')
-  } else if (/product/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/product.php') {
     $('#product-page').addClass('active')
     $('#product-submenu').collapse('show')
-  } else if (/notify-repair/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/notify-repair.php') {
     $('#notify-repair-page').addClass('active')
     $('#repair-submenu').collapse('show')
-  } else if (/approve-repair/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/approve-repair.php') {
     $('#approve-repair-page').addClass('active')
     $('#repair-submenu').collapse('show')
-  } else if (/assign-repair/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/assign-repair.php') {
     $('#assign-repair-page').addClass('active')
     $('#repair-submenu').collapse('show')
-  } else if (/finish-repair/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/finish-repair.php') {
     $('#finish-repair-page').addClass('active')
     $('#repair-submenu').collapse('show')
-  } else if (/mat-with/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/mat-with.php') {
     $('#mat-with-page').addClass('active')
     $('#material-submenu').collapse('show')
-  } else if (/mat-with-approve/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/mat-with-approve.php') {
     $('#mat-with-approve-page').addClass('active')
     $('#material-submenu').collapse('show')
-  } else if (/product-report/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/product-report.php') {
     $('#product-report-page').addClass('active')
     $('#report-submenu').collapse('show')
-  } else if (/product-history-report/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/product-history-report.php') {
     $('#product-history-report-page').addClass('active')
     $('#report-submenu').collapse('show')
-  } else if (/product-repair-report/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/product-repair-report.php') {
     $('#product-repair-report-page').addClass('active')
     $('#report-submenu').collapse('show')
-  } else if (/product-inactive-report/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/product-inactive-report.php') {
     $('#product-inactive-report-page').addClass('active')
     $('#report-submenu').collapse('show')
-  } else if (/mat-with-report/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/mat-with-report.php') {
     $('#mat-with-report-page').addClass('active')
     $('#report-submenu').collapse('show')
-  } else if (/mat-use-report/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/mat-use-report.php') {
     $('#mat-use-report-page').addClass('active')
     $('#report-submenu').collapse('show')
-  } else if (/mat-with-approve-report/ig.test(pathname)) {
+  } else if (pathname === '/photak-system/pages/mat-with-approve-report.php') {
     $('#mat-with-approve-report-page').addClass('active')
     $('#report-submenu').collapse('show')
   } else {
