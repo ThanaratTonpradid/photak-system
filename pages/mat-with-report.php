cb43014b60feb1f562d5c80c70f6f05f8616ba24
Thanarat Tonpradit <auauone@gmail.com>
2020-07-19 22:54:47 +0700

    edit detail input and table title

--- pages/mat-with-report.php
+++ pages/mat-with-report.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-  <title>รายงานทะเบียน - ระบบแจ้งซ่อมครุภัณฑ์คอมพิวเตอร์</title>
+  <title>รายงานการเบิกวัสดุ - ระบบแจ้งซ่อมครุภัณฑ์คอมพิวเตอร์</title>
   <?php include '../components/head.php'; ?>
   <link rel="stylesheet" href="/photak-system/assets/css/dashboard.css">
 </head>
@@ -14,42 +14,36 @@
       <?php include '../components/sidebar.php'; ?>
       <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
         <div class="d-flex justify-content-center flex-wrap flex-column align-items-center pt-3 pb-2 mb-3">
-          <h3>รายงานทะเบียน</h3>
+          <h3>รายงานการเบิกวัสดุ</h3>
           <h4>โรงพยาบาลโพธิ์ตาก</h4>
-          <!-- <form>
-            <div class="form-group row">
-              <label for="inputPassword" class="col-sm-3 col-form-label">ณ วันที่</label>
-              <div class="col-sm-9">
-                <input type="date" class="form-control" id="inputPassword">
+          <form>
+            <div class="form-group input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text">ระหว่างวันที่</span>
               </div>
+              <input type="date" class="form-control" id="startDate">
+              <div class="input-group-prepend">
+                <span class="input-group-text">ถึง</span>
+              </div>
+              <input type="date" class="form-control" id="endDate">
             </div>
-          </form> -->
+          </form>
         </div>
         <div class="table-responsive">
           <table class="table table-striped table-sm">
             <thead>
               <tr>
                 <th>ลำดับ</th>
-                <th>รายการ</th>
-                <th>หมายเลขครุภัณฑ์</th>
+                <th>รหัสวัสดุ</th>
+                <th>ชื่อวัสดุ</th>
                 <th>ประเภท</th>
-                <th>ยี่ห้อ</th>
-                <th>อาคาร</th>
-                <th>ห้อง</th>
-                <th>สถานะ</th>
-                <th>วันที่เริ่มใช้</th>
-                <th>ผู้รับผิดชอบ</th>
+                <th>จำนวน</th>
               </tr>
             </thead>
             <tbody>
               <tr>
                 <td>1,001</td>
                 <td>Lorem</td>
-                <td>ipsum</td>
-                <td>ipsum</td>
-                <td>ipsum</td>
-                <td>ipsum</td>
-                <td>ipsum</td>
                 <td>ipsum</td>
                 <td>ipsum</td>
                 <td>ipsum</td>
@@ -60,20 +54,10 @@
                 <td>consectetur</td>
                 <td>consectetur</td>
                 <td>consectetur</td>
-                <td>consectetur</td>
-                <td>consectetur</td>
-                <td>consectetur</td>
-                <td>consectetur</td>
-                <td>consectetur</td>
               </tr>
               <tr>
                 <td>1,003</td>
                 <td>Integer</td>
-                <td>nec</td>
-                <td>nec</td>
-                <td>nec</td>
-                <td>nec</td>
-                <td>nec</td>
                 <td>nec</td>
                 <td>nec</td>
                 <td>nec</td>
@@ -84,20 +68,10 @@
                 <td>Sed</td>
                 <td>Sed</td>
                 <td>Sed</td>
-                <td>Sed</td>
-                <td>Sed</td>
-                <td>Sed</td>
-                <td>Sed</td>
-                <td>Sed</td>
               </tr>
               <tr>
                 <td>1,004</td>
                 <td>dapibus</td>
-                <td>diam</td>
-                <td>diam</td>
-                <td>diam</td>
-                <td>diam</td>
-                <td>diam</td>
                 <td>diam</td>
                 <td>diam</td>
                 <td>diam</td>
@@ -108,20 +82,10 @@
                 <td>quis</td>
                 <td>quis</td>
                 <td>quis</td>
-                <td>quis</td>
-                <td>quis</td>
-                <td>quis</td>
-                <td>quis</td>
-                <td>quis</td>
               </tr>
               <tr>
                 <td>1,006</td>
                 <td>nibh</td>
-                <td>elementum</td>
-                <td>elementum</td>
-                <td>elementum</td>
-                <td>elementum</td>
-                <td>elementum</td>
                 <td>elementum</td>
                 <td>elementum</td>
                 <td>elementum</td>
