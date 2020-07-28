5cc10f1b19db3d5a79e7edc94ec9c7139e8564ec
Thanarat Tonpradit <auauone@gmail.com>
2020-07-29 02:53:56 +0700

    change src

--- components/footer-script.php
+++ components/footer-script.php
@@ -1,8 +1,8 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
 <!--   Jquery   -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
 <!--   Popper   -->
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <!--   Bootstrap JS Files   -->
-<script src="/photak-system/assets/js/bootstrap/bootstrap.bundle.js"></script>
+<script src="/photak-system/assets/js/bootstrap/bootstrap.bundle.min.js"></script>
 <script src="/photak-system/assets/js/dashboard.js"></script>
