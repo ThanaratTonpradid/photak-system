c5cbb2d7a6da983d8cc05f49547fdc6c56dfa0e9
Thanarat Tonpradit <auauone@gmail.com>
2020-07-29 03:53:43 +0700

    add crud for building

---
+++ actions/building.php
@@ -0,0 +1,45 @@
+<?php
+  include  'db-connection.php';
+  if(count($_POST)>0){
+    if($_POST['type']=='create'){
+      $buil_name=$_POST['buil_name'];
+      $sql = "INSERT INTO `building`( `buil_name`) VALUES ('$buil_name')";
+      if (mysqli_query($conn, $sql)) {
+        echo json_encode(array("statusCode"=>200));
+      }
+      else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+      }
+      mysqli_close($conn);
+    }
+  }
+
+  if(count($_POST)>0){
+    if($_POST['type']=='update'){
+      $id=$_POST['id'];
+      $buil_name=$_POST['buil_name'];
+      $sql = "UPDATE `building` SET `buil_name`='$buil_name' WHERE id=$id";
+      if (mysqli_query($conn, $sql)) {
+        echo json_encode(array("statusCode"=>200));
+      }
+      else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+      }
+      mysqli_close($conn);
+    }
+  }
+
+  if(count($_POST)>0){
+    if($_POST['type']=='delete'){
+      $id=$_POST['id'];
+      $sql = "DELETE FROM building WHERE id=$id";
+      if (mysqli_query($conn, $sql)) {
+        echo json_encode(array("statusCode"=>200));
+      }
+      else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+      }
+      mysqli_close($conn);
+    }
+  }
+?>
--- pages/building.php
+++ pages/building.php
@@ -18,6 +18,7 @@
         <button id="add-form-btn" type="button" class="btn btn-primary"><span data-feather="plus"></span>เพิ่มข้อมูล</button>
       </div>
       <div class="table-responsive">
+        <?php include  '../actions/db-connection.php'; ?>
         <table class="table table-striped table-sm">
           <thead>
             <tr>
@@ -28,117 +29,102 @@
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1,001</td>
-              <td>Lorem</td>
-              <td>ipsum</td>
+            <?php
+              $result = mysqli_query($conn,"SELECT * FROM building");
+              $i=1;
+              if (mysqli_num_rows($result) > 0) {
+                while($row = mysqli_fetch_array($result)) {
+            ?>
+            <tr id="<?php echo $row["id"]; ?>">
+              <td><?php echo $i; ?></td>
+              <td><?php echo $row["id"]; ?></td>
+              <td><?php echo $row["buil_name"]; ?></td>
               <td>
-                <button type="button" class="btn btn-sm btn-info" onclick="handleEditRow()"><span data-feather="edit-2"></button>
-                <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteBuildingDialog"><span data-feather="trash-2"></button>
+                <button
+                  type="button"
+                  class="btn btn-sm btn-info edit-btn"
+                  data-id="<?php echo $row["id"]; ?>"
+                  data-buil-name="<?php echo $row["buil_name"]; ?>"
+                >
+                  <span data-feather="edit-2">
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-sm btn-danger delete-btn"
+                  data-toggle="modal"
+                  data-target="#deleteDialog"
+                  data-id="<?php echo $row["id"]; ?>"
+                  data-buil-name="<?php echo $row["buil_name"]; ?>"
+                >
+                    <span data-feather="trash-2">
+                  </button>
               </td>
             </tr>
+            <?php
+                  $i++;
+                }
+              } else {
+            ?>
             <tr>
-              <td>1,002</td>
-              <td>amet</td>
-              <td>consectetur</td>
-              <td>
-                <button type="button" class="btn btn-sm btn-info" onclick="handleEditRow()"><span data-feather="edit-2"></button>
-                <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteBuildingDialog"><span data-feather="trash-2"></button>
+              <td colspan="4" class="text-center">
+                <span>ไม่พบข้อมูล</span>
               </td>
             </tr>
-            <tr>
-              <td>1,003</td>
-              <td>Integer</td>
-              <td>nec</td>
-              <td>
-                <button type="button" class="btn btn-sm btn-info" onclick="handleEditRow()"><span data-feather="edit-2"></button>
-                <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteBuildingDialog"><span data-feather="trash-2"></button>
-              </td>
-            </tr>
-            <tr>
-              <td>1,003</td>
-              <td>libero</td>
-              <td>Sed</td>
-              <td>
-                <button type="button" class="btn btn-sm btn-info" onclick="handleEditRow()"><span data-feather="edit-2"></button>
-                <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteBuildingDialog"><span data-feather="trash-2"></button>
-              </td>
-            </tr>
-            <tr>
-              <td>1,004</td>
-              <td>dapibus</td>
-              <td>diam</td>
-              <td>
-                <button type="button" class="btn btn-sm btn-info" onclick="handleEditRow()"><span data-feather="edit-2"></button>
-                <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteBuildingDialog"><span data-feather="trash-2"></button>
-              </td>
-            </tr>
-            <tr>
-              <td>1,005</td>
-              <td>Nulla</td>
-              <td>quis</td>
-              <td>
-                <button type="button" class="btn btn-sm btn-info" onclick="handleEditRow()"><span data-feather="edit-2"></button>
-                <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteBuildingDialog"><span data-feather="trash-2"></button>
-              </td>
-            </tr>
-            <tr>
-              <td>1,006</td>
-              <td>nibh</td>
-              <td>elementum</td>
-              <td>
-                <button type="button" class="btn btn-sm btn-info" onclick="handleEditRow()"><span data-feather="edit-2"></button>
-                <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteBuildingDialog"><span data-feather="trash-2"></button>
-              </td>
-            </tr>
+            <?php
+              }
+            ?>
           </tbody>
         </table>
       </div>
       <!-- Create Form -->
-      <div class="create-building-form d-none">
+      <div class="create-form d-none">
         <h4>เพิ่มอาคาร</h4>
-        <form>
+        <form id="create-form">
           <div class="form-row">
             <div class="col-md-4 mb-3">
-              <label for="validateName">ชื่ออาคาร</label>
-              <input type="text" class="form-control" id="validateName" required>
+              <label for="buil_name">ชื่ออาคาร</label>
+              <input type="text" class="form-control" id="buil_name" name="buil_name" required>
             </div>
           </div>
+          <input type="hidden" value="create" name="type">
           <button id="cancel-save-btn" class="btn btn-secondary" type="button">ยกเลิก</button>
-          <button id="save-btn" class="btn btn-primary" type="submit">เพิ่มข้อมูล</button>
+          <button id="save-btn" class="btn btn-primary" type="button">เพิ่มข้อมูล</button>
         </form>
       </div>
       <!-- Update Form -->
-      <div class="edit-building-form d-none">
+      <div class="edit-form d-none">
         <h4>แก้ไขอาคาร</h4>
-        <form>
+        <form id="edit-from">
           <div class="form-row">
             <div class="col-md-4 mb-3">
-              <label for="validateName">ชื่ออาคาร</label>
-              <input type="text" class="form-control" id="validateName" required>
+              <label for="buil_name_u">ชื่ออาคาร</label>
+              <input type="hidden" id="buil_id_u" name="id" class="form-control" required>
+              <input type="text" class="form-control" id="buil_name_u" name="buil_name" required>
             </div>
           </div>
+          <input type="hidden" value="update" name="type">
           <button id="cancel-edit-btn" class="btn btn-secondary" type="button">ยกเลิก</button>
-          <button id="edit-btn" class="btn btn-primary" type="submit">แก้ไขข้อมูล</button>
+          <button id="edit-btn" class="btn btn-primary" type="button">แก้ไขข้อมูล</button>
         </form>
       </div>
 
       <!-- Delete Modal -->
-      <div class="modal fade" id="deleteBuildingDialog" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal fade" id="deleteDialog" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
-              <h4 class="modal-title" id="deleteBuildingDialogLabel">ยืนยันการลบข้อมูล</h4>
+              <h4 class="modal-title" id="deleteDialogLabel">ยืนยันการลบข้อมูล</h4>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
             <div class="modal-body">
-              ท่านต้องการลบข้อมูล /photak-system. หรือไม่
+              <input type="hidden" id="id_d" name="id" class="form-control">
+              ท่านต้องการลบข้อมูล <span id="buil_name_d"></span> หรือไม่
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-dismiss="modal">ยกเลิก</button>
-              <button type="button" class="btn btn-danger">ลบข้อมูล</button>
+              <button type="button" class="btn btn-danger" id="delete-btn">ลบข้อมูล</button>
             </div>
           </div>
         </div>
