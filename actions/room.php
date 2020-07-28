04b0382b0be63d945dafb06684089f9b2247b3fd
Thanarat Tonpradit <auauone@gmail.com>
2020-07-29 05:00:10 +0700

    add crud for room

---
+++ actions/room.php
@@ -0,0 +1,49 @@
+<?php
+  include  'db-connection.php';
+  if(count($_POST)>0){
+    if($_POST['type']=='create'){
+      $room_name=$_POST['room_name'];
+      $room_phone=$_POST['room_phone'];
+      $buil_id=(int)$_POST['buil_id'];
+      $sql = "INSERT INTO `room`( `room_name`,`room_phone`,`buil_id`) VALUES ('$room_name','$room_phone','$buil_id')";
+      if (mysqli_query($conn, $sql)) {
+        echo json_encode(array("statusCode"=>200));
+      }
+      else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+      }
+      mysqli_close($conn);
+    }
+  }
+
+  if(count($_POST)>0){
+    if($_POST['type']=='update'){
+      $id=$_POST['id'];
+      $room_name=$_POST['room_name'];
+      $room_phone=$_POST['room_phone'];
+      $buil_id=(int)$_POST['buil_id'];
+      $sql = "UPDATE `room` SET `room_name`='$room_name',`room_phone`='$room_phone',`buil_id`='$buil_id' WHERE id=$id";
+      if (mysqli_query($conn, $sql)) {
+        echo json_encode(array("statusCode"=>200));
+      }
+      else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+      }
+      mysqli_close($conn);
+    }
+  }
+
+  if(count($_POST)>0){
+    if($_POST['type']=='delete'){
+      $id=$_POST['id'];
+      $sql = "DELETE FROM room WHERE id=$id";
+      if (mysqli_query($conn, $sql)) {
+        echo json_encode(array("statusCode"=>200));
+      }
+      else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+      }
+      mysqli_close($conn);
+    }
+  }
+?>
--- pages/room.php
+++ pages/room.php
@@ -18,181 +18,169 @@
         <button id="add-form-btn" type="button" class="btn btn-primary"><span data-feather="plus"></span>เพิ่มข้อมูล</button>
       </div>
       <div class="table-responsive">
+        <?php include  '../actions/db-connection.php'; ?>
         <table class="table table-striped table-sm">
           <thead>
             <tr>
               <th>ลำดับ</th>
               <th>รหัส</th>
               <th>ชื่อห้อง</th>
-              <th>อาคาร</th>
               <th>เบอร์โทร</th>
+              <th>อาคาร</th>
               <th></th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1,001</td>
-              <td>Lorem</td>
-              <td>ipsum</td>
-              <td>1</td>
-              <td>0897654321</td>
+            <?php
+              $result = mysqli_query($conn,"SELECT * FROM room");
+              $i=1;
+              if (mysqli_num_rows($result) > 0) {
+                while($row = mysqli_fetch_array($result)) {
+            ?>
+            <tr id="<?php echo $row["id"]; ?>">
+              <td><?php echo $i; ?></td>
+              <td><?php echo $row["id"]; ?></td>
+              <td><?php echo $row["room_name"]; ?></td>
+              <td><?php echo $row["room_phone"]; ?></td>
+              <td><?php echo $row["buil_id"]; ?></td>
               <td>
-                <button type="button" class="btn btn-sm btn-info" onclick="handleEditRow()"><span data-feather="edit-2"></button>
-                <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteRoomDialog"><span data-feather="trash-2"></button>
+                <button
+                  type="button"
+                  class="btn btn-sm btn-info edit-btn"
+                  data-id="<?php echo $row["id"]; ?>"
+                  data-room-name="<?php echo $row["room_name"]; ?>"
+                  data-room-phone="<?php echo $row["room_phone"]; ?>"
+                  data-buil-id="<?php echo $row["buil_id"]; ?>"
+                >
+                  <span data-feather="edit-2">
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-sm btn-danger delete-btn"
+                  data-toggle="modal"
+                  data-target="#deleteDialog"
+                  data-id="<?php echo $row["id"]; ?>"
+                  data-room-name="<?php echo $row["room_name"]; ?>"
+                  data-room-phone="<?php echo $row["room_phone"]; ?>"
+                  data-buil-id="<?php echo $row["buil_id"]; ?>"
+                >
+                    <span data-feather="trash-2">
+                  </button>
               </td>
             </tr>
+            <?php
+                  $i++;
+                }
+              } else {
+            ?>
             <tr>
-              <td>1,002</td>
-              <td>amet</td>
-              <td>consectetur</td>
-              <td>2</td>
-              <td>0897654321</td>
-              <td>
-                <button type="button" class="btn btn-sm btn-info" onclick="handleEditRow()"><span data-feather="edit-2"></button>
-                <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteRoomDialog"><span data-feather="trash-2"></button>
+              <td colspan="6" class="text-center">
+                <span>ไม่พบข้อมูล</span>
               </td>
             </tr>
-            <tr>
-              <td>1,003</td>
-              <td>Integer</td>
-              <td>nec</td>
-              <td>3</td>
-              <td>0897654321</td>
-              <td>
-                <button type="button" class="btn btn-sm btn-info" onclick="handleEditRow()"><span data-feather="edit-2"></button>
-                <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteRoomDialog"><span data-feather="trash-2"></button>
-              </td>
-            </tr>
-            <tr>
-              <td>1,003</td>
-              <td>libero</td>
-              <td>Sed</td>
-              <td>4</td>
-              <td>0897654321</td>
-              <td>
-                <button type="button" class="btn btn-sm btn-info" onclick="handleEditRow()"><span data-feather="edit-2"></button>
-                <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteRoomDialog"><span data-feather="trash-2"></button>
-              </td>
-            </tr>
-            <tr>
-              <td>1,004</td>
-              <td>dapibus</td>
-              <td>diam</td>
-              <td>5</td>
-              <td>0897654321</td>
-              <td>
-                <button type="button" class="btn btn-sm btn-info" onclick="handleEditRow()"><span data-feather="edit-2"></button>
-                <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteRoomDialog"><span data-feather="trash-2"></button>
-              </td>
-            </tr>
-            <tr>
-              <td>1,005</td>
-              <td>Nulla</td>
-              <td>quis</td>
-              <td>6</td>
-              <td>0897654321</td>
-              <td>
-                <button type="button" class="btn btn-sm btn-info" onclick="handleEditRow()"><span data-feather="edit-2"></button>
-                <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteRoomDialog"><span data-feather="trash-2"></button>
-              </td>
-            </tr>
-            <tr>
-              <td>1,006</td>
-              <td>nibh</td>
-              <td>elementum</td>
-              <td>7</td>
-              <td>0897654321</td>
-              <td>
-                <button type="button" class="btn btn-sm btn-info" onclick="handleEditRow()"><span data-feather="edit-2"></button>
-                <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteRoomDialog"><span data-feather="trash-2"></button>
-              </td>
-            </tr>
+            <?php
+              }
+            ?>
           </tbody>
         </table>
       </div>
       <!-- Create Form -->
-      <div class="create-room-form d-none">
+      <div class="create-form d-none">
         <h4>เพิ่มห้อง</h4>
-        <form>
+        <form id="create-form">
           <div class="form-row">
             <div class="col-md-4 mb-3">
-              <label for="validateName">ชื่อห้อง</label>
-              <input type="text" class="form-control" id="validateName" required>
+              <label for="room_name">ชื่อห้อง</label>
+              <input type="text" class="form-control" id="room_name" name="room_name" required>
             </div>
           </div>
           <div class="form-row">
             <div class="col-md-4 mb-3">
-              <label for="validateBuilding">อาคาร</label>
-              <select class="form-control" id="validateBuilding" required>
-                <option selected disabled value="">เลือกอาคาร/photak-system.</option>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
+              <label for="room_phone">เบอร์โทร</label>
+              <input type="text" class="form-control" id="room_phone" name="room_phone" required>
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="col-md-4 mb-3">
+              <label for="buil_id">อาคาร</label>
+              <select class="form-control" id="buil_id" name="buil_id" required>
+                <option selected disabled value="">เลือกอาคาร</option>
+                <?php
+                  $result = mysqli_query($conn,"SELECT * FROM building");
+                  if (mysqli_num_rows($result) > 0) {
+                    while($row = mysqli_fetch_array($result)) {
+                ?>
+                <option value="<?php echo $row["id"]; ?>"><?php echo $row["buil_name"]; ?></option>
+                <?php
+                    }
+                  }
+                ?>
               </select>
             </div>
           </div>
-          <div class="form-row">
-            <div class="col-md-4 mb-3">
-              <label for="validatePhone">เบอร์โทร</label>
-              <input type="text" class="form-control" id="validatePhone" required>
-            </div>
-          </div>
+          <input type="hidden" value="create" name="type">
           <button id="cancel-save-btn" class="btn btn-secondary" type="button">ยกเลิก</button>
-          <button id="save-btn" class="btn btn-primary" type="submit">เพิ่มข้อมูล</button>
+          <button id="save-btn" class="btn btn-primary" type="button">เพิ่มข้อมูล</button>
         </form>
       </div>
       <!-- Update Form -->
-      <div class="edit-room-form d-none">
+      <div class="edit-form d-none">
         <h4>แก้ไขห้อง</h4>
-        <form>
+        <form id="edit-form">
           <div class="form-row">
             <div class="col-md-4 mb-3">
-              <label for="validateName">ชื่อห้อง</label>
-              <input type="text" class="form-control" id="validateName" required>
+              <label for="room_name">ชื่อห้อง</label>
+              <input type="hidden" id="room_id_u" name="id" class="form-control" required>
+              <input type="text" class="form-control" id="room_name_u" name="room_name" required>
             </div>
           </div>
           <div class="form-row">
             <div class="col-md-4 mb-3">
-              <label for="validateBuilding">อาคาร</label>
-              <select class="form-control" id="validateBuilding" required>
-                <option selected disabled value="">เลือกอาคาร/photak-system.</option>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
+              <label for="room_phone">เบอร์โทร</label>
+              <input type="text" class="form-control" id="room_phone_u" name="room_phone" required>
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="col-md-4 mb-3">
+              <label for="buil_id">อาคาร</label>
+              <select class="form-control" id="buil_id_u" name="buil_id" required>
+                <option selected disabled value="">เลือกอาคาร</option>
+                <?php
+                  $result = mysqli_query($conn,"SELECT * FROM building");
+                  if (mysqli_num_rows($result) > 0) {
+                    while($row = mysqli_fetch_array($result)) {
+                ?>
+                <option value="<?php echo $row["id"]; ?>"><?php echo $row["buil_name"]; ?></option>
+                <?php
+                    }
+                  }
+                ?>
               </select>
             </div>
           </div>
-          <div class="form-row">
-            <div class="col-md-4 mb-3">
-              <label for="validatePhone">เบอร์โทร</label>
-              <input type="text" class="form-control" id="validatePhone" required>
-            </div>
-          </div>
+          <input type="hidden" value="update" name="type">
           <button id="cancel-edit-btn" class="btn btn-secondary" type="button">ยกเลิก</button>
-          <button id="edit-btn" class="btn btn-primary" type="submit">แก้ไขข้อมูล</button>
+          <button id="edit-btn" class="btn btn-primary" type="button">แก้ไขข้อมูล</button>
         </form>
       </div>
 
       <!-- Delete Modal -->
-      <div class="modal fade" id="deleteRoomDialog" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal fade" id="deleteDialog" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
-              <h4 class="modal-title" id="deleteRoomDialogLabel">ยืนยันการลบข้อมูล</h4>
+              <h4 class="modal-title" id="deleteDialogLabel">ยืนยันการลบข้อมูล</h4>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
             <div class="modal-body">
-              ท่านต้องการลบข้อมูล /photak-system. หรือไม่
+              <input type="hidden" id="id_d" name="id" class="form-control">
+              ท่านต้องการลบข้อมูล <span id="room_name_d"></span> หรือไม่
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-dismiss="modal">ยกเลิก</button>
-              <button type="button" class="btn btn-danger">ลบข้อมูล</button>
+              <button type="button" class="btn btn-danger" id="delete-btn">ลบข้อมูล</button>
             </div>
           </div>
         </div>
