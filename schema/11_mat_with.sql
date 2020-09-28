e2ce74ff62513b2c7de691ca90b43a911040f13f
Thanarat Tonpradit <thanarat@dollarsign.co>
2020-09-29 01:47:16 +0700

    change schema

--- schema/11_mat_with.sql
+++ schema/11_mat_with.sql
@@ -2,9 +2,10 @@ USE `photak`;
 
 CREATE TABLE mat_with (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    mat_id int, FOREIGN KEY (mat_id) REFERENCES material (id),
-    date_approve DATETIME NOT NULL,
-    approve_mw VARCHAR(50) NOT NULL,
+    mat_with_name VARCHAR(50) NOT NULL,
+    date_take DATETIME NULL,
+    date_approve DATETIME NULL,
+    approve_mw VARCHAR(50) NULL,
     em_take INT NULL,
     em_approver INT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
--- schema/12_mat_with_detail.sql
+++ schema/12_mat_with_detail.sql
@@ -1,7 +1,8 @@
 USE `photak`;
 
 CREATE TABLE mat_with_detail (
-    mat_with_id int, FOREIGN KEY (mat_with_id) REFERENCES material (id),
-    mat_id int, FOREIGN KEY (mat_id) REFERENCES employee (id),
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    mat_with_id int, FOREIGN KEY (mat_with_id) REFERENCES mat_with (id),
+    mat_id int, FOREIGN KEY (mat_id) REFERENCES material (id),
     quantity CHAR NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
